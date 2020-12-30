00fd242b9f398dbfbe0fc26aab05ef99fb5e6351
Shadow <ShadowNight573@users.noreply.github.com>
2020-12-30 17:59:38 -0600

    changing specialservers code

--- stdcommands/specialservers/specialservers.go
+++ stdcommands/specialservers/specialservers.go
@@ -17,18 +17,33 @@ var Command = &commands.YAGCommand{
     Name:                 "specialservers",
     Description:          ";))",
     HideFromHelp:         true,
-    RunFunc: util.RequireOwner(func(data *dcmd.Data) (interface{}, error) {
-        var whitelisted []int64
-        err := common.RedisPool.Do(radix.Cmd(&whitelisted, "SMEMBERS", "special_servers"))
-        if err != nil {
-            return "", err
-        }
+    Arguments: []*dcmd.ArgDef{
+      &dcmd.ArgDef{Name: "Skip", Help: "Entries to skip", Type: dcmd.Int, Default: 0},
+    },
+    RunFunc: func(data *dcmd.Data) (interface{}, error) {
+      offset := data.Args[0].Int()
 
-        out := "**List Of Whitelisted Servers:**\n"
-        for _, v := range whitelisted {
-            out += fmt.Sprintf("%d\n", v)
-        }
+      var whitelisted []int64
+      err := common.RedisPool.Do(radix.Cmd(&whitelisted, "SMEMBERS", "special_servers"))
+      if err != nil {
+          return "", err
+      }
+      
+      results, err := models.JoinedGuilds(
+        qm.Where("left_at is null"), // Don't include guilds that we left
+        qm.WhereIn("id", whitelisted...), // Only whitelisted guilds
+        qm.OrderBy("id desc"), // Needed so we have consistent output
+        qm.Limit(10), // Limit to 10 results
+        qm.Offset(offset),
+      ).AllG(data.Context())
+      if err != nil {
+        return "", err
+      }
 
-        return out, nil
-    }),
+      resp := "**Whitelisted servers**\n"
+      for _, v := range results {
+        resp += fmt.Sprintf("`%d`: **%s**\n", v.ID, v.Name)
+      }
+      return resp, nil
+    },
 }
