27d447b177f4d61069ce747495f2a3ad7679d074
Shadow <ShadowNight573@users.noreply.github.com>
2020-12-30 17:39:38 -0600

    fix(?) 3

--- stdcommands/specialservers/specialservers.go
+++ stdcommands/specialservers/specialservers.go
@@ -1,27 +1,34 @@
 package specialservers
 
 import (
-	"github.com/jonas747/dcmd"
-	"github.com/jonas747/yagpdb/commands"
-	"github.com/jonas747/yagpdb/common"
-	"github.com/jonas747/yagpdb/stdcommands/util"
-	"github.com/mediocregopher/radix/v3"
+    "fmt"
+
+    "github.com/jonas747/dcmd"
+    "github.com/jonas747/yagpdb/commands"
+    "github.com/jonas747/yagpdb/common"
+    "github.com/jonas747/yagpdb/stdcommands/util"
+    "github.com/mediocregopher/radix/v3"
 )
 
 var Command = &commands.YAGCommand{
-	Cooldown:             2,
-	CmdCategory:          commands.CategoryDebug,
-	HideFromCommandsPage: true,
-	Name:                 "specialservers",
-	Description:          ";))",
-	HideFromHelp:         true,
-	RunFunc: util.RequireOwner(func(data *dcmd.Data) (interface{}, error) {
-			var whitelisted []int64
-			err := common.RedisPool.Do(radix.Cmd(&whitelisted, "SMEMBERS", "special_servers"))
-			if err != nil {
-				return "", err
-			}
-			
-			return "**List Of Whitelisted Servers:**\n" + string(whitelisted), nil
-	}),
+    Cooldown:             2,
+    CmdCategory:          commands.CategoryDebug,
+    HideFromCommandsPage: true,
+    Name:                 "specialservers",
+    Description:          ";))",
+    HideFromHelp:         true,
+    RunFunc: util.RequireOwner(func(data *dcmd.Data) (interface{}, error) {
+        var whitelisted []int64
+        err := common.RedisPool.Do(radix.Cmd(&whitelisted, "SMEMBERS", "special_servers"))
+        if err != nil {
+            return "", err
+        }
+
+        out := "**List Of Whitelisted Servers:**\n"
+        for _, v := range whitelisted {
+            out += fmt.Sprintf("%d\n", v)
+        }
+
+        return out, nil
+    }),
 }
