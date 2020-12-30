94d5479c2827e31e69ce16204e7bca117b2fbe22
Shadow <ShadowNight573@users.noreply.github.com>
2020-12-29 20:46:52 -0600

    try again

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -1065,6 +1065,7 @@ func (c *Context) tmplGetChannel(channel interface{}) (*CtxChannel, error) {
 	if cstate == nil {
 		return nil, errors.New("Channel not in state")
 	}
+	
 	CtxChannelFromCS(cstate), nil
 }
 
