37b038070cef79b39a9fb79c2c785cb1ce37f0b1
Shadow <ShadowNight573@users.noreply.github.com>
2021-01-02 16:55:58 -0600

    forgot to import some stuff
    listroles fix

--- stdcommands/listroles/listroles.go
+++ stdcommands/listroles/listroles.go
@@ -3,11 +3,13 @@ package listroles
 import (
 	"fmt"
 	"sort"
+	"bytes"
 
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/discordgo"
 	"github.com/jonas747/dutil"
 	"github.com/jonas747/yagpdb/commands"
+	"github.com/jonas747/yagpdb/common"
 )
 
 var Command = &commands.YAGCommand{
