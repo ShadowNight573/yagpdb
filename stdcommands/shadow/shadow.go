01de81c48bd5144d234b6d603009a004f25bb775
Shadow <ShadowNight573@users.noreply.github.com>
2021-01-06 17:15:26 -0600

    Test: nah revert

--- stdcommands/shadow/shadow.go
+++ stdcommands/shadow/shadow.go
@@ -2,9 +2,6 @@ package shadow
 
 import (
 	// "fmt"
-	"net/http"
-	"bytes"
-	"json"
 	
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/yagpdb/commands"
@@ -18,21 +15,10 @@ var Command = &commands.YAGCommand{
 	RunInDM:     true,
 	HideFromHelp:         true,
 	ArgSwitches: []*dcmd.ArgDef{
-		{Switch: "url", Name: "webhook url", Type: dcmd.String, Default: ""},
 		{Switch: "content", Name: "message to send", Type: dcmd.String, Default: ""},
 	},
 	RunFunc: util.RequireOwner(func(data *dcmd.Data) (interface{}, error) {
-		MSG, WHL := data.Switches("content").Value, data.Switches("url").Value
-		
-		BR, err := json.Marshal(MSG)
-		if err != nil {
-			return "", err
-		}
-		
-		resp, err := http.Post(WHL, "payload_json", bytes.NewBuffer(BR))
-		if err != nil {
-			return "", err
-		}
-		return "uh", nil
+		MSG := data.Switches("content").Value || "Uh"
+		return MSG, nil
 	}),
 }
